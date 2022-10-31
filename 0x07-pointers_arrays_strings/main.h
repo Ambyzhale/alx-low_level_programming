3f5a5b5cada3d801c782c5de02115f47c8130694
Ambyzhale <ambroseoyar@gmail.com>
2022-10-31 22:28:19 +0000

    main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
-#ifndef : MAIN.H
-#define : MAIN.H
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
@@ -10,4 +8,3 @@ char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
-#endif :
