7aa1f5cd0da64ce85e4482d79f9f7768baeade48
Ambyzhale <ambroseoyar@gmail.com>
2022-11-02 18:09:30 +0000

    0-puts_recursion

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int helper(int a, int b);
+int primeFinder(int a, int b);
+int palindromeChecker(char *str, int len, int i);
