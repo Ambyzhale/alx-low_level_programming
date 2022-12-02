a2c9f196d89f962acf2a5bba5dc85281dd371a02
Ambyzhale <ambroseoyar@gmail.com>
2022-12-02 00:43:15 +0000

    binary manipulation

--- 0x14-bit_manipulation/0-binary_to_unit.c
+++ 0x14-bit_manipulation/0-binary_to_unit.c
@@ -9,25 +9,25 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int num, i;
+	unsigned int num, i, expo = 1;
 
 	if (!b || !*b)
 		return (0);
 
-	num = i = 0;
+	i = 0;
 	while (b[i])
 	{
-		if (b[i] > 49)
+		if (b[i] - 48 > 1)
 			return (0);
-		else if (b[i] == 49)
-		{
-			num <<= 1;
-			num += 1;
-		}
-		else
-			num <<= 1;
 		i++;
 	}
+	i--;
+	num = 0;
+	do {
+		num += (b[i] - '0') * expo;
+		expo *= 2;
+	} while (i--);
+
 	return (num);
 }
 
