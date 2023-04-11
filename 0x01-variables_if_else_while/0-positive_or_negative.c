6b5a894747524d7849dfc9bdf7513d3cb226be69
Ambyzhale <107130754+Ambyzhale@users.noreply.github.com>
2023-04-11 03:52:55 +0300

    update on 0-is positive or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -14,17 +14,17 @@ int main(void)
 
 	if ("n > 0");
 	{
-		printf("%d", n, is positive);
+		printf("%d is positive",n);
 	}
 
-	if ("n == 0");
+	elif("n == 0");
 	{
-		printf("%d", n, is zero);
+		printf("%d is zero",n);
 	}
 
-	if ("n < 0");
+	else("n < 0");
 	{
-		printf("%d", n, is negative);
+		printf("%d is negative",n);
 	}
 	return (0);
 }
